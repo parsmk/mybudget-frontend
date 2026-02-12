930ef9ddcfaccf74e87ce6321c85126fef7141c8
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-12 14:51:45 -0800

    feat: proxy now refreshes token

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ROUTES } from "./app/_route-map";
 
-const proxy = (request: NextRequest) => {
-  console.log(request.cookies.get("accessToken"));
-  if (!request.cookies.has("accessToken")) {
+const proxy = async (request: NextRequest) => {
+  if (request.cookies.has("accessToken")) return NextResponse.next();
+
+  const refresh = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/refresh`, {
+    method: "POST",
+    headers: {
+      cookie: request.headers.get("cookie") ?? "",
+    },
+  });
+
+  if (!refresh.ok)
     return NextResponse.redirect(new URL(ROUTES.HOME, request.nextUrl));
+
+  const response = NextResponse.next();
+  const setCookie = refresh.headers.get("set-cookie");
+  if (setCookie) {
+    response.headers.append("set-cookie", setCookie);
   }
 
   return NextResponse.next();
