efdec43fddcbe6984ddd32850dca568d34cf7e07
Parsa MK <p.mohtashami.k@gmail.com>
2026-02-16 17:51:22 -0800

    feat: dashboard greeting card

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -2,6 +2,7 @@
 
 import { AccountsCard } from "@/components/dashboard/AccountsCard";
 import { CategoriesCard } from "@/components/dashboard/CategoriesCard";
+import { DashboardCard } from "@/components/dashboard/DashboardCard";
 import { useAccounts } from "@/hooks/accounts/useAccounts";
 import { useCategories } from "@/hooks/categories/useCategories";
 
@@ -10,6 +11,9 @@ const PortalLandingPage = () => {
   const { data: accounts } = useAccounts();
   return (
     <div className="h-full grid gap-10 grid-cols-2 grid-rows-2 p-10">
+      <div className="col-span-full">
+        <DashboardCard>Welcome user!</DashboardCard>
+      </div>
       <CategoriesCard categories={categories} />
       <AccountsCard accounts={accounts} />
     </div>
--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -2,12 +2,14 @@ export const DashboardCard = ({
   title,
   children,
 }: {
-  title: string;
+  title?: string;
   children: React.ReactNode;
 }) => {
   return (
     <div className="bg-foreground/10 overflow-y-auto p-5 w-full h-full rounded-md">
-      <h2 className="text-lg border-b-1 border-primary/50 p-3">{title}</h2>
+      {title && (
+        <h2 className="text-lg border-b-1 border-primary/50 p-3">{title}</h2>
+      )}
       {children}
     </div>
   );
