d9dde7aa73e2733e3a6213b01134e6b35443ac9f
Parsa MK <p.mohtashami.k@gmail.com>
2026-03-07 17:39:04 -0800

    feat: Tooltip component

---
+++ components/ui-kit/Tooltip.tsx
@@ -0,0 +1,35 @@
+import { createPortal } from "react-dom";
+
+type Variants = "danger" | "default";
+
+type TooltipProps = {
+  children: React.ReactNode;
+  target: React.RefObject<Element | null>;
+  open: boolean;
+  variant?: Variants;
+};
+
+export const Tooltip = ({
+  children,
+  target,
+  open,
+  variant = "default",
+}: TooltipProps) => {
+  if (!open) return;
+
+  const variantClasses: Record<Variants, string> = {
+    danger: "bg-danger/90 border-2 border-danger text-white",
+    default: "",
+  };
+
+  const pos = target.current?.getBoundingClientRect();
+  return createPortal(
+    <div
+      style={{ top: pos?.top, left: pos?.left }}
+      className={`fixed p-2 -translate-y-[105%] z-50 rounded-md min-w-50 min-h-20 ${variantClasses[variant]}`}
+    >
+      {children}
+    </div>,
+    document.body,
+  );
+};
